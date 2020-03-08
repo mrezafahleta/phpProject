8f20a487fdccf9b85beb8a4c585f1c4c18e1b306
M.Reza Fahleta <mrezafahleta1997@gmail.com>
2020-03-08 09:38:17 +0700

    perubahan karir

--- header.php
+++ header.php
@@ -9,9 +9,9 @@
          <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
             <div class="navbar-nav">
                <a class="nav-item nav-link active" href="?page=about">TENTANG KAMI SEMUA <span class="sr-only">(current)</span></a>
-               <a class="nav-item nav-link" href="?page=business">BISNIS KAMI</a>
+               <a class="nav-item nav-link" href="?page=business">BISNIS KAMI </a>
                <a class="nav-item nav-link" href="?page=relationship">HUBUNGAN INVESTOR</a>
-               <a class="nav-item nav-link" href="?page=carrier">KARIR</a>
+               <a class="nav-item nav-link" href="?page=carrier">KARIR PEKERJAAN</a>
                <a class="nav-item nav-link" href="?page=contact">HUBUNGI KAMI</a>
                <a href=""></a>
             </div>
